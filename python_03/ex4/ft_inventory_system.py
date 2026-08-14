49c6496cff328a065f1195ee177bd0d060b3c514
Aymene Chafai <aymenchafai350@gmail.com>
2026-08-14 10:34:13 +0200

    ex 0, 1, 4

--- python_03/ex4/ft_inventory_system.py
+++ python_03/ex4/ft_inventory_system.py
@@ -1,5 +1,26 @@
 import sys
 
-inputs: dict = dict(a.split(':') for a in sys.argv[1:])
 
-print(inputs)
+def inventory_parsing() -> dict:
+    inventory: dict = dict()
+    seen: set = set()
+    for arg in sys.argv[1:]:
+        if ':' not in arg:
+            print(f"Error - invalid parameter '{arg}'")
+        key, value_str = arg.split(':')
+        try:
+            value = int(value_str)
+        except Exception:
+            print(f"Quantity error for '{key}':", end=' ')
+            print(f"invalid literal for int() with base 10: '{value}'")
+            continue
+        if key in seen:
+            print(f"Redundant item {key} - discarding")
+            continue
+        seen.add(key)
+        inventory[key] = value
+    return inventory
+
+
+if __name__ == "__main__":
+    print("=== Inventory System Analysis ===")
