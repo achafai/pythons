e0380c2659e93d278771dc81388b3f4106a34aab
Aymene Chafai <aymenchafai350@gmail.com>
2026-08-18 17:07:28 +0200

    done with ex5 python3

--- python_03/ex5/ft_data_stream.py
+++ python_03/ex5/ft_data_stream.py
@@ -9,6 +9,12 @@ def gen_event(players: list, actions: list) -> typing.Generator:
         yield (name, action)
 
 
+def consume_event(events: list) -> typing.Generator:
+    while events:
+        event_index = random.randrange(len(events))
+        yield events.pop(event_index)
+
+
 if __name__ == "__main__":
     players = ["bob", "alice", "dylan", "charlie"]
     actions = ["run", "eat", "sleep", "grup", "run",
@@ -17,7 +23,12 @@ if __name__ == "__main__":
     for i in range(1000):
         name, action = next(gen_event(players, actions))
         print(f"Event {i}: Player {name} did action {action}")
+    print()
     events = []
     for i in range(10):
         events.append(next(gen_event(players, actions)))
     print(f"Built list of 10 events: {events}")
+    print()
+    for event in consume_event(events):
+        print(f"Got event from list: {event}")
+        print(f"Remains in list: {events}")
